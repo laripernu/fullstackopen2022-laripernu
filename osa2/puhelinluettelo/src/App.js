e697eefe7daf6bb4beac0bb00cda8fdf4cc04f2b
laripernu <lari.pernu@hotmail.com>
2022-07-05 11:17:15 +0300

    create puhelinluettelo app and finish step1

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const Name = ({person}) => {
+    return (
+      <div>
+        {person.name}
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => 
+            <Name key={person.name} person={person} />
+        )}
+      </div>
+    </div>
+  )
+
+}
+
+export default App
